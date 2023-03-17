6f689d918d8c69b35d559ea1330a3e93e19974ef
Yuting Yan <745668379@qq.com>
2023-03-17 20:35:52 +0800

    set proxy and add paper revise prompt

--- app.py
+++ app.py
@@ -2,18 +2,25 @@ import os
 
 import openai
 from flask import Flask, redirect, render_template, request, url_for
+import socks
+import socket
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+# set proxy
+socks.set_default_proxy(socks.SOCKS5, "localhost", 1080)
+socket.socket = socks.socksocket
 
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
         animal = request.form["animal"]
+        # passage = request.form["passage"]
         response = openai.Completion.create(
             model="text-davinci-003",
             prompt=generate_prompt(animal),
+            # prompt=revise_prompt(passage),
             temperature=0.6,
         )
         return redirect(url_for("index", result=response.choices[0].text))
@@ -33,3 +40,8 @@ Animal: {}
 Names:""".format(
         animal.capitalize()
     )
+
+def revise_prompt(passage):
+    return """Please proofread and polish the passage from an academic angle and highlight the modification:
+
+    {}""".format(passage)
